996718ae2cfbd47db7bd52c269a4a2a3e183aac6
JoshuaMoelans <joshua.moelans@student.uantwerpen.be>
2023-02-16 19:35:25 +0100

    Added avatars and some styling

--- static/tablescript.js
+++ static/tablescript.js
@@ -5,62 +5,87 @@ var tableData;
 async function loadData() {
     const stations_url = 'https://63998da716b0fdad77409a5e.mockapi.io/api/v1/hikers';
     const response = await fetch(stations_url)
-    tableData = JSON.parse(await response.text())
+    if(response.ok){
+            tableData = JSON.parse(await response.text())
+        return 0;
+    }
+    else{
+        console.log("Error: " + response.status);
+        return -1;
+    }
 }
 
 async function onDocumentLoad(){
-    await loadData();
+    let result = await loadData();
+    if(result !== 0){
+        window.alert("Error loading data. Please refresh the page.")
+        return;
+    }
     console.log(tableData);
-    //Create a HTML Table element.
-                    var table = document.getElementById("dataTable");
+    var table = document.getElementById("dataTable");
 
-                    //Add the header row.
-                    var row = table.insertRow(-1);
+    //Add the header row.
+    var row = table.insertRow(-1);
+    row.id = "headerRow";
 
-                    //Add the header cells.
-                    var headerCell = document.createElement("TH");
-                    headerCell.innerHTML = "NAME";
-                    row.appendChild(headerCell);
+    //Add the header cells.
+    var headerCell = document.createElement("TH");
+    headerCell.innerHTML = "";
+    row.appendChild(headerCell);
 
-                    headerCell = document.createElement("TH");
-                    headerCell.innerHTML = "DATE OF BIRTH";
-                    row.appendChild(headerCell);
+    headerCell = document.createElement("TH");
+    headerCell.innerHTML = "NAME";
+    row.appendChild(headerCell);
 
-                    headerCell = document.createElement("TH");
-                    headerCell.innerHTML = "CITY";
-                    row.appendChild(headerCell);
+    headerCell = document.createElement("TH");
+    headerCell.innerHTML = "DATE OF BIRTH";
+    row.appendChild(headerCell);
 
-                    headerCell = document.createElement("TH");
-                    headerCell.innerHTML = "COUNTRY";
-                    row.appendChild(headerCell);
+    headerCell = document.createElement("TH");
+    headerCell.innerHTML = "CITY";
+    row.appendChild(headerCell);
 
-                    //Add the data rows.
-                    for (var i = 0; i < tableData.length; i++) {
-                        //Add the data row.
-                        var row = table.insertRow(-1);
+    headerCell = document.createElement("TH");
+    headerCell.innerHTML = "COUNTRY";
+    row.appendChild(headerCell);
 
-                        //Add the data cells.
-                        var cell = row.insertCell(-1);
-                        cell.innerHTML = tableData[i].name;
+    headerCell = document.createElement("TH");
+    headerCell.innerHTML = "";
+    row.appendChild(headerCell);
+    //Add the data rows.
+    for (var i = 0; i < tableData.length; i++) {
+        //Add the data row.
+        var row = table.insertRow(-1);
 
-                        cell = row.insertCell(-1);
-                        let DOB = new Date(tableData[i].dateOfBirth);
-                        cell.innerHTML = DOB.toLocaleDateString();
+        //Add the data cells.
+        var cell = row.insertCell(-1);
+        var img = document.createElement('img');
+        img.src = tableData[i].avatar;
+        img.id = "avatar"
+        cell.appendChild(img)
 
-                        cell = row.insertCell(-1);
-                        cell.innerHTML = tableData[i].country;
+        cell = row.insertCell(-1);
+        cell.innerHTML = tableData[i].name;
 
-                        cell = row.insertCell(-1);
-                        cell.innerHTML = tableData[i].city;
+        cell = row.insertCell(-1);
+        let DOB = new Date(tableData[i].dateOfBirth);
+        cell.innerHTML = DOB.toLocaleDateString();
 
-                        cell = row.insertCell(-1);
-                        cell.addEventListener("click", function(e) {
-                            console.log("Delete row");
-                            e.currentTarget.parentNode.remove();
-                        });
-                        cell.id = "deleteButton";
-                        cell.innerHTML = "🗑️"
-                    }
+        cell = row.insertCell(-1);
+        cell.innerHTML = tableData[i].country;
 
-                    document.appendChild(table);
+        cell = row.insertCell(-1);
+        cell.innerHTML = tableData[i].city;
+
+        cell = row.insertCell(-1);
+        cell.addEventListener("click", function(e) {
+            if(confirm("Are you sure you want to delete this row?")){
+                e.currentTarget.parentNode.remove();
+            }
+        });
+        cell.id = "deleteButton";
+        cell.innerHTML = "🗑️"
+    }
+
+    document.appendChild(table);
 }
