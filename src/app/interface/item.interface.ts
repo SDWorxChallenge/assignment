1ac1acd270bafeac9dfd12e812543f3fc71884c3
IVAN KULIASHOV <42475953+Krivetka@users.noreply.github.com>
2023-02-16 21:08:28 +0100

    SD Worx Challenge! 🚀

---
+++ src/app/interface/item.interface.ts
@@ -0,0 +1,8 @@
+export interface ItemInterface {
+  "dateOfBirth": string,
+  "name": string,
+  "avatar": string,
+  "country": string,
+  "city": string,
+  "id": string
+}
---
+++ src/app/state/state.ts
@@ -0,0 +1,6 @@
+import {ItemInterface} from "../interface/item.interface";
+
+
+function getData():void {
+  getData.subscribe((data: any)=>data = data);
+}
